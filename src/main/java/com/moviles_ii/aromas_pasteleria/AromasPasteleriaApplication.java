c49034d84fccbe7fc7b78cf8b55aad77e79b9abc
diegoacernac <diegoacernac@gmail.com>
2023-11-09 13:01:44 -0500

    Create project

---
+++ src/main/java/com/moviles_ii/aromas_pasteleria/AromasPasteleriaApplication.java
@@ -0,0 +1,13 @@
+package com.moviles_ii.aromas_pasteleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromasPasteleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromasPasteleriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviles_ii/aromas_pasteleria/AromasPasteleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviles_ii.aromas_pasteleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AromasPasteleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
